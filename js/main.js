5d8173c962e2d903d632a1f8ab5a046e96fba227
Wolf3Dnet <nikwolf1997@mail.ru>
2021-07-22 15:37:24 +0300

    add redacting function

--- js/main.js
+++ js/main.js
@@ -2,10 +2,6 @@ const input = document.getElementById("input");
 const btnAdd = document.getElementById("btn-add");
 const elements = document.getElementById("elements");
 
-console.log(input);
-console.log(btnAdd);
-console.log(elements);
-
 btnAdd.addEventListener("click", (e) => {
   if (input.value === "") return;
   createOrDeleteElement(input.value);
@@ -18,6 +14,19 @@ function createOrDeleteElement(value) {
   const span = document.createElement("span");
   const liRadio = document.createElement("button");
 
+  const liInput = document.createElement("textarea");
+  const liInputAdd = document.createElement("button");
+  const liInputUndo = document.createElement("button");
+
+  liInputAdd.className = "li-input-add";
+  liInputAdd.textContent = "+";
+
+  liInputUndo.className = "li-input-undo";
+  liInputUndo.textContent = "-";
+
+  liInput.className = "li-input";
+  liInput.value = input.value;
+
   li.className = "li";
 
   span.textContent = value;
@@ -28,7 +37,7 @@ function createOrDeleteElement(value) {
 
   liRadio.className = "li-radio";
 
-  li.append(liRadio, span, liBtn);
+  li.append(liRadio, span, liBtn, liInputAdd, liInputUndo, liInput);
   elements.append(li);
 
   liBtn.addEventListener("click", (e) => {
@@ -37,5 +46,22 @@ function createOrDeleteElement(value) {
 
   liRadio.addEventListener("click", (e) => {
     li.classList.toggle("li-active");
+  });
+
+  span.addEventListener("click", (e) => {
+    li.classList.add("li-change");
+    liRadio.setAttribute("disabled", true);
+  });
+
+  liInputAdd.addEventListener("click", (e) => {
+    span.textContent = liInput.value;
+    liRadio.removeAttribute("disabled", false);
+    li.classList.remove("li-change");
+  });
+
+  liInputUndo.addEventListener("click", (e) => {
+    liRadio.removeAttribute("disabled", false);
+    liInput.value = span.textContent;
+    li.classList.remove("li-change");
   });
 }
